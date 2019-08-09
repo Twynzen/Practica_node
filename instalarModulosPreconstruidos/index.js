e5a740147658ccc2df85074b0a6853ef8f07eda3
Raziel <razzziel001@gmail.com>
2019-08-09 18:24:02 -0500

    Unpar de funciones para ver el espacio libre en memoria, tengo error al actualizar atom, consiedarar cambiar de entorno para desarrollar

--- instalarModulosPreconstruidos/index.js
+++ instalarModulosPreconstruidos/index.js
@@ -1,2 +1,10 @@
+
 const os = require('os');
 console.log(os.platform());
+console.log(os.release());
+//Memoria libre en bits
+console.log('free mem: ', os.freemem());
+//Memoria total en bits
+console.log('total mem: ', os.totalmem());
+
+//Vere modulso filesistem
