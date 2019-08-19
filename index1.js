742d5a8c9da31974490ab6d576c6d9f83cb7320d
Raziel <razzziel001@gmail.com>
2019-08-19 17:57:03 -0500

    Instalacion de modulo colors que genera un node_modules y un package.json para administrar y saber donde estan las dependencias agregadas

--- index1.js
+++ index1.js
@@ -1,4 +1,5 @@
 const http = require ('http');
+const colors = require('colors');
 
 const handleServer = function (req, res){
   res.writeHead(200, {'Content-type':'text/plain'});
@@ -9,6 +10,9 @@ const handleServer = function (req, res){
 const server = http.createServer(handleServer);
 
 server.listen(3000, function(){
-  console.log('Server on port 3000');
+  console.log('Server on port 3000'.red);
 });
 // aprendere NPM para la proxima
+//Instale el modulo colors con npm install, y se creo el node_modules que se encarga de administrar eso
+//el pacakage.json se crea para indicar de donde saco el modulo instalado
+//con el npm install instala las dependencias automaticamente al leer el package.json
