e8c96e749ededaa212637a4604641d19ff4f6303
Raziel <razzziel001@gmail.com>
2019-08-21 21:29:13 -0500

    Creación de servidor con express

--- index1.js
+++ index1.js
@@ -1,3 +1,4 @@
+/*
 const http = require ('http');
 const colors = require('colors');
 
@@ -12,7 +13,22 @@ const server = http.createServer(handleServer);
 server.listen(3000, function(){
   console.log('Server on port 3000'.red);
 });
+*/
 // aprendere NPM para la proxima
 //Instale el modulo colors con npm install, y se creo el node_modules que se encarga de administrar eso
 //el pacakage.json se crea para indicar de donde saco el modulo instalado
 //con el npm install instala las dependencias automaticamente al leer el package.json
+
+//lo mismo pero con express
+const express = require('express');
+const server = express();
+const color = require('colors');
+
+server.get('/', function(req, res){
+  res.send('<h1>Hola pues con express y node</h1>');
+  res.end(); 
+})
+
+server.listen(3000, function(){
+  console.log('Server on port 3000'.red);
+});
